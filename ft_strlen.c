053ebdf62c14ae9e7085abbe88713854d078140d
Danilo Mataveli <dmatavel@c1t2s1.42.rio>
2022-05-10 16:50:56 -0300

    ft_strlen tested & libft.h updated + norm ok

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,29 +6,21 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 18:55:12 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/04/05 20:44:40 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/05/10 16:45:51 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char	*str)
+#include "libft.h"
+
+size_t	ft_strlen(const char	*s)
 {
 	int	count;
 
 	count = 0;
-	while (*str)
+	while (*s)
 	{
-		str++;
+		s++;
 		count++;
 	}
 	return (count);
 }
-
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("%d", ft_strlen("Hello, World!"));
-	return (0);
-}
-*/
--- libft.h
+++ libft.h
@@ -6,19 +6,21 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 13:27:04 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/05/10 14:58:33 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/05/10 16:49:08 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <unistd.h>
+
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
-int ft_toupper(int c);
-int ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 
 #endif
