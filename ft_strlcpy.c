e51b54174c7cee2792833bd8f76b6cf91147b6eb
dmatavel <matavelidanilo@gmail.com>
2022-05-13 14:00:26 -0300

    ft_strlcpy tested + norm ok & libft.h updt

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,36 +6,25 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 12:34:35 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/05/12 16:47:01 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/05/13 13:57:14 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	
+
 	i = 0;
-	while (i < size - 1 && src[i] != '\0')
+	if (size > 0)
 	{
-		dst[i] = src[i];
-		i++;
+		while (i < size - 1 && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
 	}
-	while (i < size - 1)
-	{
-		dst[i] = '\0';
-	}
+	dst[i] = '\0';
 	return (ft_strlen(src));
 }
-
-int	main(void)
-{
-	char	dst[14] = "Hello, 42!";
-
-	ft_strlcpy(dst, "Hello, World!", 14);
-	printf("%s\n", dst);
-	return (0);
-}
-
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 13:27:04 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/05/12 23:55:24 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/05/13 13:59:12 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # include <unistd.h>
 
-/* ctype.h functions */
-
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
@@ -24,9 +22,7 @@ int	ft_isascii(int c);
 int	ft_isprint(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
-
-/* string.h functions */
-
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
