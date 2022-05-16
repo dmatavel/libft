0fb0e61c3034a9c9e68926ad1f26996fcdff839e
dmatavel <matavelidanilo@gmail.com>
2022-05-16 18:46:41 -0300

    ft_strlcat done & prototyped in libft.h

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,37 +6,28 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 14:09:45 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/05/15 16:20:08 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/05/16 18:43:16 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dst_len = ft_strlen(dst);
+	size_t	dst_len;
+	size_t	src_len;
 	size_t	i;
-	
+	size_t	ret;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
 	i = 0;
 	while (i < size - 1 && src[i] != '\0')
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
-	dst[dst_len +i] = '\0';
-	return (ft_strlen((dst_len + src)));
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	string[] = "42";
-	char	buffer[10] = "Hello!";
-	
-	ft_strlcat(buffer, string, 1);
-	printf("%s\n", buffer);
-	printf("%ld\n", ft_strlcat(buffer, string, 13));
-	return (0);
+	dst[dst_len + i] = '\0';
+	ret = dst_len + src_len;
+	return (ret);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 13:27:04 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/05/13 17:22:13 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/05/16 18:44:13 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
