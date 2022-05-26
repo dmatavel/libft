7971d05d14739cbcf4eb8ef58a9b97b9778bc1b2
dmatavel <matavelidanilo@gmail.com>
2022-05-26 16:50:13 -0300

    ft_strlcat norm ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,45 +6,27 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 14:09:45 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/05/25 18:00:39 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/05/26 15:33:45 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dest_len;
-	size_t	src_len;
 	size_t	i;
+	size_t	j;
+	size_t	dst_len;
+	size_t	src_len;
 
-	dest_len = ft_strlen(dest);
+	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	if (size <= dest_len)
+	if (size <= dst_len)
 		return (src_len + size);
 	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[dest_len++] = src[i++];
-		if (i == size - (dest_len - 1))
-			break;
-	}
-	dest[dest_len] = '\0';
-	return (dest_len + src_len);
+	j = dst_len;
+	while (src[i] && j < size - 1)
+		dst[j++] = src[i++];
+	dst[j] = '\0';
+	return (dst_len + src_len);
 }
-
-/*
-int	main(void)
-{
-	char	src[] = "anotherstring";
-	char	dstsize[20] = "one_string";
-	size_t	n;
-
-	n = 20;
-	printf("%ld\n", (ft_strlcat(dstsize, src, n)));
-	printf("%s\n", dstsize);
-	return (0);
-}
-*/
-
