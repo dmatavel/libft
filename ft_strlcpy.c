1aa2af1892a5ef2f0e171a9cabe284eabc6d454e
dmatavel <matavelidanilo@gmail.com>
2022-05-26 16:49:25 -0300

    ft_strlcpy norm ok

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 12:34:35 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/05/19 16:01:48 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/05/26 15:35:07 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	
+
 	i = 0;
 	if (size > 0)
 	{
