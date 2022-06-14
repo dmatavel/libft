ed43f6f43a1d474e718775fe811aeb950aefad2e
dmatavel <matavelidanilo@gmail.com>
2022-06-14 05:09:32 -0300

    better variables names

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 13:46:33 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/06/13 21:16:47 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/06/14 05:08:37 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
  *   ft_strtrim allocates (with malloc(3)) and returns a copy of 's1'
  *   with the characters specified in 'set' removed from the beginning
  *   and the end of the string.
+ *   
  *   Parameters:
  *   's1' is the string to be trimmed and 'set' the reference set of
  *   characters to trim.
+ *   
  *   Return values:
  *   The trimmed string or NULL if allocation fails.
  */
@@ -28,8 +30,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	unsigned int	start;
 	unsigned int	last;
-	unsigned int	i;
-	unsigned int	j;
+	unsigned int	start_i;
+	unsigned int	last_i;
 	size_t			len;
 
 	if (!s1 || !set)
@@ -41,9 +43,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (ft_strchr(set, s1[last]) != NULL)
 			last--;
 	len = 0;
-	i = start;
-	j = last;
-	while (i++ <= j)
+	start_i = start;
+	last_i = last;
+	while (start_i++ <= last_i)
 		len++;
 	return (ft_substr(s1, start, len));
 }
