b418cb565c42ec43f7c39cc40937a022a873fa60
Danilo Mataveli <dmatavel@pop-os.localdomain>
2023-01-10 18:35:58 -0300

    Fix identation at line 27

--- src/extends/ft_stoarr.c
+++ src/extends/ft_stoarr.c
@@ -6,7 +6,7 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 12:52:50 by dmatavel          #+#    #+#             */
-/*   Updated: 2023/01/10 18:31:14 by dmatavel         ###   ########.fr       */
+/*   Updated: 2023/01/10 18:35:33 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ int	*ft_stoarr(int size, char **str)
 	if (arr == NULL)
 		return (NULL);
 	while (0 < size--)
-			arr[i++] = atoi(str[j++]);
+		arr[i++] = atoi(str[j++]);
 	return (arr);
 }
