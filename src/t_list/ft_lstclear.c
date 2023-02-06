4c6179a1b3e63a04ad005a209a37993fb52aaf9c
Danilo Mataveli <dmatavel@pop-os.localdomain>
2023-02-06 16:43:33 -0300

    Add ft_lstclear

--- src/t_list/ft_lstclear.c
+++ src/t_list/ft_lstclear.c
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   t_list.h                                           :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/22 13:38:01 by dmatavel          #+#    #+#             */
-/*   Updated: 2023/02/06 16:40:27 by dmatavel         ###   ########.fr       */
+/*   Created: 2023/02/06 16:36:35 by dmatavel          #+#    #+#             */
+/*   Updated: 2023/02/06 16:41:54 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef T_LIST_H
-# define T_LIST_H
+#include "../../include/libft.h"
 
-typedef struct s_list
+void	ft_lstclear(t_list **lst)
 {
-	int				content;
-	int				index;
-	struct s_list	*next;
-}					t_list;
+	t_list	*tmp;
+	size_t	size;
 
-t_list	*ft_lstnew(int content);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-size_t	ft_lstsize(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_printlst(t_list *lst);
-void	ft_lstclear(t_list **lst);
-
-#endif //T_LIST_H
+	size = ft_lstsize(*lst);
+	while (size--)
+	{
+		tmp = *lst;
+		*lst = (*lst)->next;
+		free(tmp);
+		tmp = NULL;
+	}
+}
