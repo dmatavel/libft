04ff78e6194b5d08c6ec30b90234a03821e2e957
dmatavel <matavelidanilo@gmail.com>
2023-05-11 21:54:33 -0300

    remove ft_printlst; remove index from s_list struct

--- src/t_list/ft_lstnew.c
+++ src/t_list/ft_lstnew.c
@@ -6,13 +6,13 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 13:55:43 by dmatavel          #+#    #+#             */
-/*   Updated: 2023/02/02 13:29:30 by dmatavel         ###   ########.fr       */
+/*   Updated: 2023/05/11 21:53:39 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/libft.h"
 
-t_list	*ft_lstnew(int content)
+t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
 
@@ -20,7 +20,6 @@ t_list	*ft_lstnew(int content)
 	if (!new)
 		return (NULL);
 	new->content = content;
-	new->index = 0;
 	new->next = NULL;
 	return (new);
 }
